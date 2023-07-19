421d64c2d5c04e022cf36c15f27fe8d65bc84dbe
Yaser Elgizawy <113913404+YASir5555@users.noreply.github.com>
2023-07-19 14:57:34 +0300

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/#Challenge 1.py
@@ -0,0 +1,15 @@
+#Challenge 1 
+
+# A message for the user
+message = "This is going to be tricky ;)"
+
+ # show the message on the screen
+print(message)
+
+
+ # Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+# =8
+result = 5 - 3
+# =2
---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/#Challenge 2.py
@@ -0,0 +1,12 @@
+#Challenge 2 
+
+#Ask the user :"What's my favorite food?"
+food = "meat"
+
+if food == "meat" : 
+    print ("Yep! So amazing!")
+
+else : 
+    print("Yuck! That's not it")
+
+print("Thanks for playing")
