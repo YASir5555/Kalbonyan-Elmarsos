d3b230dcf0e7638ee8d79afd527c37c361ca4cd6
Yaser Elgizawy <113913404+YASir5555@users.noreply.github.com>
2023-07-22 11:40:46 +0300

    Add files via upload

---
+++ 02-Udemy/-02-Js-Maximilian/Code/assignments/dom-ASSIGNMENT-start/assignment.js
@@ -0,0 +1,21 @@
+const task = document.getElementById('task-1');
+task.style.backgroundColor = 'black';
+task.style.color = 'white';
+
+const task2 = document.querySelector('li');
+task2.style.backgroundColor = 'black';
+task2.style.color = 'white';
+
+// ////
+const title = document.head.querySelector('title');
+title.textContent = 'Assignment -Solved!';
+
+const title2 = document.querySelector('title');
+title2.textContent = 'Assignment -Solved!';
+
+// ////
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment -Solved!';
+
+const h12 = document.getElementsByTagName('h1');
+h12[0].textContent = 'Assignment -Solved!';
